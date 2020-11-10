b8a5accffd4432ffda488c0a54f83ab7d2f24358
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2020-11-10 11:22:54 -0500

    Started from `rtos_task_management.c`. Set up GPIOs and implemented LED blinker with `vTaskDelay`.

---
+++ lab08_freertos_software_timer.sdk/rtos_software_timer/Debug/src/rtos_software_timer.d
@@ -0,0 +1,86 @@
+src/rtos_software_timer.o src/rtos_software_timer.o: \
+ ../src/rtos_software_timer.c \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/FreeRTOS.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/projdefs.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/portable.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/deprecated_definitions.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/portmacro.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/mpu_wrappers.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/task.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/list.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/queue.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/task.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/timers.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xstatus.h
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/FreeRTOS.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/projdefs.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/portable.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/deprecated_definitions.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/portmacro.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/mpu_wrappers.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/task.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/list.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/queue.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/task.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/timers.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../rtos_software_timer_bsp/ps7_cortexa9_0/include/xstatus.h:
---
+++ lab08_freertos_software_timer.sdk/rtos_software_timer/Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LD_SRCS += \
+../src/lscript.ld 
+
+C_SRCS += \
+../src/rtos_software_timer.c 
+
+OBJS += \
+./src/rtos_software_timer.o 
+
+C_DEPS += \
+./src/rtos_software_timer.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM v7 gcc compiler'
+	arm-none-eabi-gcc -Wall -O0 -g3 -c -fmessage-length=0 -MT"$@" -mcpu=cortex-a9 -mfpu=vfpv3 -mfloat-abi=hard -I../../rtos_software_timer_bsp/ps7_cortexa9_0/include -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
